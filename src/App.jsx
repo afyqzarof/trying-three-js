50b606820d65f45cf1ecebafc6923d2b19d5d936
afyqeyzar <norafyqe@gmail.com>
2024-02-19 22:04:34 +0000

    feat: intial setups

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
-function App() {
-  return <h1>trying three.js</h1>;
-}
+import "./App.scss";
+import * as THREE from "three";
+import { useEffect, useRef } from "react";
 
+function App() {
+  const refContainer = useRef(null);
+  useEffect(() => {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    refContainer.current &&
+      refContainer.current.appendChild(renderer.domElement);
+    // === THREE.JS CODE START ===
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(0, 0, 100);
+    camera.lookAt(0, 0, 0);
+    const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    const points = [];
+    points.push(new THREE.Vector3(-10, 0, 0));
+    points.push(new THREE.Vector3(0, 10, 0));
+    points.push(new THREE.Vector3(10, 0, 0));
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    // var geometry = new THREE.BoxGeometry(1, 1, 1);
+    // var material = new THREE.MeshBasicMaterial({ color: 0xedff00 });
+    const line = new THREE.Line(geometry, material);
+    var cube = new THREE.Mesh(geometry, material);
+
+    scene.add(line);
+    const animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+  }, []);
+  return <div ref={refContainer}></div>;
+}
 export default App;
